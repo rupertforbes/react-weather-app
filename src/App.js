a705893362b8a3f7e3e483e6fd6e36c6704c0477
rupertforbes <127436772+rupertforbes@users.noreply.github.com>
2023-11-10 16:07:08 +0000

    Created new react app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Weather from "./Weather";
+import axios from "axios";
 
 function App() {
+  let [temp, setTemp] = useState(null);
+
+  function showData(response) {
+    setTemp(response.data.main.temp);
+    console.log(response);
+  }
+
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=1fd8093fa5ff12d796d7de756cc9d6b9`;
+  axios.get(url).then(showData);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello from React</h1>
+        <p>The temperature in London is {Math.round(temp)}°C</p>
+        <Weather />
       </header>
     </div>
   );
